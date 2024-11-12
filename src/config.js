a4f4a316c9b0abc86d521123853c4ed39274d676
isaac-kellogg-quantellia <isaac.kellogg@quantellia.com>
2024-11-12 14:08:47 -0500

    Allow JointJS to be taller
    Adjust config.js: JointJS height tries to be 90k pixels tall.
    This is then limited by the style of its parent container. See style.css

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export class Config {
     //Canvas size
     static paperWidth = "64%"; //See style.css for .right, which is taking up the other 34%.
-    static paperHeight = 900;
+    static paperHeight = 90000; //Effectively caps max height at 90k pixels (see style.css). Hopefully that covers most use cases.
 
     //Used for resizing elements
     static minElementWidth = 120;
