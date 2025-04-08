349d35081f64c91f1793f3c876abd0d49cd14ac8
S. Isaac Kellogg <isaac.kellogg@quantellia.com>
2025-04-08 09:25:09 -0400

    New Element improvements and fixes
    - Fixed naming for dependencies created by New Element:
    Name for the existing element was not being access correctly, resulting in the dependency
    always having "Unnamed" for the existing element. Name was also created with the wrong
    order / arrow direction
    - New elements created when other elements are selected will be placed near those elements
    - Added comments for `addNewElement` and `fuzzInt`

--- src/lib/elementCRUD.tsx
+++ src/lib/elementCRUD.tsx
@@ -1,5 +1,18 @@
 import { v4 as uuidv4 } from 'uuid';
 
+/**
+ * Pure/immutable: Updates the given model JSON to include a new element.
+ * If elements are selected in the selectionBuffer, this new element will be connected
+ * to those selected elements, and placed near them. If not, this element will be placed
+ * around a default location (100, 250).
+ * Does not mutate model JSON. Instead, returns the updated JSON with the new element added.
+ * @param model Model JSON to add the element to
+ * @param selectionBuffer Array of UUIDs for selected diagram elements
+ * @param setSelectionBuffer The React useState set function for selectionBuffer
+ * @param diagramElementMap Map from element UUIDs to their JSON data
+ * @param diagramIndex Index for which diagram we're using in the model JSON
+ * @returns Updated model JSON, with the new element added
+ */
 export function addNewElement(model: any, selectionBuffer: Array<string>, setSelectionBuffer: Function, diagramElementMap: Map<string, any>, diagramIndex = 0)
 {
     let workingModel = structuredClone(model);
@@ -8,6 +21,15 @@ export function addNewElement(model: any, selectionBuffer: Array<string>, setSel
         const defaultX = 100;
         const defaultY = 250;
         
+        /**
+         * Slightly randomizes an int. Returns a number near intToFuzz, in a range
+         * of width fuzzAmount, centered at intToFuzz.
+         * For slightly randomizing the placement of the new element, so that unconnected
+         * elements placed one after the other won't hide each other.
+         * @param intToFuzz Base number. Resulting number will average around this value.
+         * @param fuzzAmount The width of the fuzz range.
+         * @returns Fuzzed int in the range (intToFuzz - (fuzzAmount / 2)) to (intToFuzz + (fuzzAmount / 2))
+         */
         const fuzzInt = (intToFuzz = 0, fuzzAmount = 25) => {
             const max = intToFuzz + (fuzzAmount / 2);
             const min = intToFuzz - (fuzzAmount / 2)
@@ -15,8 +37,26 @@ export function addNewElement(model: any, selectionBuffer: Array<string>, setSel
             return Math.floor(Math.random() * (max - min + 1)) + min;
         }
 
-        const newElementX = fuzzInt(defaultX);
-        const newElementY = fuzzInt(defaultY);
+        let newElementX = fuzzInt(defaultX);
+        let newElementY = fuzzInt(defaultY);
+
+        if(selectionBuffer.length > 0)
+        {
+            //We're creating a connected element.
+            //Place it to the right of the selected elements,
+            //roughly in the middle vertically
+            let rightmostX = defaultX;
+            let sumOfYValues = 0;
+            selectionBuffer.forEach((selectedUUID) => {
+                const selectedJSONData = diagramElementMap.get(selectedUUID);
+                rightmostX = Math.max(rightmostX, selectedJSONData.position.x ?? 0);
+                sumOfYValues += selectedJSONData.position.y ?? 0;
+            });
+
+            const elementGap = defaultX + 300;
+            newElementX = fuzzInt(rightmostX + elementGap);
+            newElementY = fuzzInt(sumOfYValues / selectionBuffer.length); //~average Y value
+        }
 
         const newElementUUID = uuidv4();
         const newElementJSON = {
@@ -43,7 +83,7 @@ export function addNewElement(model: any, selectionBuffer: Array<string>, setSel
                 const newDependencyJSON = {
                     "meta": {
                         "uuid": newDependencyUUID,
-                        "name": `New Element --> ${diagramElementMap.get(selectedUUID).name ?? "Unnamed"}`
+                        "name": `${diagramElementMap.get(selectedUUID).meta.name ?? "Unnamed"} --> New Element`
                     },
                     "source": selectedUUID,
                     "target": newElementUUID,
